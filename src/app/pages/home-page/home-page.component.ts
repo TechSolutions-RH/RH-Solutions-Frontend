1815fa7ed6e1e5c9f3a25400a10ead6b492dd525
Gabrielfers <gabrielfer.s123@gmail.com>
2025-07-23 16:49:55 -0300

    feat: adicionar componente Section2 à página inicial com conteúdo e logo

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Section1Component } from '../../components/section1/section1.component';
+import { Section2Component } from '../../components/section2/section2.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [Section1Component],
+  imports: [Section1Component, Section2Component],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {}
