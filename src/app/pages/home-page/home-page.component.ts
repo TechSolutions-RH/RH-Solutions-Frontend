59647c167863a8825ad25a7b82972e238a5fb623
Gabrielfers <gabrielfer.s123@gmail.com>
2025-07-22 23:39:02 -0300

    feat: adicionar componente Section1 com conteúdo de boas-vindas e imagem de fundo

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { Section1Component } from '../../components/section1/section1.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  standalone: true,
+  imports: [Section1Component],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
