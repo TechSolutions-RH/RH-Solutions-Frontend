129dd83f2b451d66048f800216119a8bf38df349
Gabrielfers <gabrielfer.s123@gmail.com>
2025-07-24 15:25:55 -0300

    feat: adicionar seção de oportunidades com categorias e vagas disponíveis

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Section1Component } from '../../components/section1/section1.component';
 import { Section2Component } from '../../components/section2/section2.component';
+import { Section3Component } from '../../components/section3/section3.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [Section1Component, Section2Component],
+  imports: [Section1Component, Section2Component, Section3Component],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {}
