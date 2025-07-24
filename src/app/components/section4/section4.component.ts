98fd1d8d72d050bb3a7f39fc604bdc61142fcfa5
Gabrielfers <gabrielfer.s123@gmail.com>
2025-07-24 16:11:04 -0300

    feat: adicionar seção "Sobre Nós" com conteúdo e estilo, incluindo imagem de fundo

---
+++ src/app/components/section4/section4.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section4',
+  imports: [],
+  templateUrl: './section4.component.html',
+  styles: ``
+})
+export class Section4Component {
+
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Section1Component } from '../../components/section1/section1.component';
 import { Section2Component } from '../../components/section2/section2.component';
 import { Section3Component } from '../../components/section3/section3.component';
+import { Section4Component } from '../../components/section4/section4.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [Section1Component, Section2Component, Section3Component],
+  imports: [Section1Component, Section2Component, Section3Component, Section4Component],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {}
