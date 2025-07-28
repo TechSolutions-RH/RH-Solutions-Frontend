4f3eb1a828c62c27f5994152d6ab86bce024822c
Gabrielfers <gabrielfer.s123@gmail.com>
2025-07-28 10:15:59 -0300

    feat: remover componente de header do login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,11 +4,12 @@ import { Section2Component } from '../../components/section2/section2.component'
 import { Section3Component } from '../../components/section3/section3.component';
 import { Section4Component } from '../../components/section4/section4.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [Section1Component, Section2Component, Section3Component, Section4Component, FooterComponent],
+  imports: [Section1Component, Section2Component, Section3Component, Section4Component, FooterComponent, HeaderComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {}
