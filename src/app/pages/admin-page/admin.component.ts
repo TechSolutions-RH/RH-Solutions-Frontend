59da7a0e7a8badbfd56c9b45e39f15f3667ea0a0
Gabrielfers <gabrielfer.s123@gmail.com>
2025-08-11 10:09:03 -0300

    feat: implementar navegação por abas para colaboradores e convites no painel administrativo

--- src/app/pages/admin-page/admin.component.ts
+++ src/app/pages/admin-page/admin.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminHeaderComponent } from '../../components/admin/admin-header/admin-header.component';
+import { ColaboradorListComponent } from '../../components/admin/colaborador-list/colaborador-list.component';
+import { ConviteColaboradorComponent } from '../../components/admin/convite-colaborador/convite-colaborador.component';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [CommonModule, AdminHeaderComponent, ColaboradorListComponent, ConviteColaboradorComponent],
   templateUrl: './admin.component.html',
   styles: ``
 })
 export class AdminComponentPage {
-
+  activeTab: 'colaboradores' | 'convites' = 'colaboradores';
 }
